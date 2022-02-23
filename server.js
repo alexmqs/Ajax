4b226506b2113e2df8344ba168dd399a163edbf6
Alex Sander Luan Marques <alex.marques@sebsa.com.br>
2022-02-23 11:17:09 -0300

    Axios2

--- server.js
+++ server.js
@@ -36,5 +36,13 @@ app.post('/formulario', (req, res) => {
     })
 })
 
+app.get('/parOuImpar', (req, res) => {
+    
+    const par = parseInt(req.query.numero) % 2 === 0
+    res.send({
+        resultado: par ? 'par' : 'impar'
+    })
+})
+
 app.get('/teste', (req, res) => res.send('OK'))
 app.listen(80, () => console.log('Executando...'))
